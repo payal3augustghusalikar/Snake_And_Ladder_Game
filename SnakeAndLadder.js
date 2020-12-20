49131b936ee085761f621e0e4005336a25781cfa
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-20 21:23:33 +0530

    Added : start and winning position

--- SnakeAndLadder.js
+++ SnakeAndLadder.js
@@ -1,1 +1,6 @@
+const startPosition=0;
+const WinningPosition=100;
+
 console.log(`\n ****** Welcome to snake and ladder game ******\n`);
+console.log(`Start position is ${startPosition}`);
+console.log(`Winning position is ${WinningPosition}`);
