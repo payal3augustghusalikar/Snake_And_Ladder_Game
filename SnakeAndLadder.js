83d4f20fe8f8cb57ad0569c4b51d6e9a0bca8be6
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-21 01:42:27 +0530

    Added : winning position for play

--- SnakeAndLadder.js
+++ SnakeAndLadder.js
@@ -1,4 +1,3 @@
-
 const startPosition = 0;
 const WinningPosition = 100;
 let newPosition = startPosition;
@@ -10,29 +9,31 @@ console.log(`Start position is ${startPosition}`);
 console.log(`Winning position is ${WinningPosition} \n`);
 
 console.log(`after rolling die ->`);
-getRandomDieValue = () => randomCheck = Math.floor(Math.random() * 6) + 1;
-var RandomDieValue = getRandomDieValue();
-console.log(`die value is: ${RandomDieValue}`);
+
+while(newPosition < WinningPosition) {
+
+    getRandomDieValue = () => randomCheck = Math.floor(Math.random() * 6) + 1;
+    var RandomDieValue = getRandomDieValue();
+    console.log(`die value is: ${RandomDieValue}`);
 
     getRandomPlayValue = () => randomCheck = Math.floor(Math.random() * 3);
     var RandomPlayValue = getRandomPlayValue();
     console.log(`play value is : ${RandomPlayValue}`);
 
-        if(RandomPlayValue == 0) {
-            console.log(`no play`);
-            newPosition = newPosition;
-        }
-        else if(RandomPlayValue == 1) {
-            console.log(`got ladder`);
-         newPosition += RandomDieValue;
-        }
-        else if(RandomPlayValue == 2) {
-    
-            console.log(`snake attack`);
-            newPosition -= RandomDieValue;	
-        }
+    if(RandomPlayValue == 0) {
+        console.log(`no play`);
+        newPosition = newPosition;
+    }
+    else if(RandomPlayValue == 1) {
+        console.log(`got ladder`);
+        newPosition += RandomDieValue;
+    }
+    else if(RandomPlayValue == 2) {
+        console.log(`snake attack`);
+        newPosition -= RandomDieValue;	
+    }
 
-        console.log(`new position is : ${newPosition}`);
-
+    console.log(`new position is : ${newPosition}`);
+}
 
 
