16bb12159ff4d0d5b940b17338d0ca1fcceaeea0
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-20 21:36:00 +0530

    Added : new position using random function

--- SnakeAndLadder.js
+++ SnakeAndLadder.js
@@ -1,6 +1,13 @@
 const startPosition=0;
 const WinningPosition=100;
+let newPosition=0;
 
 console.log(`\n ****** Welcome to snake and ladder game ******\n`);
 console.log(`Start position is ${startPosition}`);
-console.log(`Winning position is ${WinningPosition}`);
+console.log(`Winning position is ${WinningPosition} \n`);
+
+getRandomDieValue = () => randomCheck = Math.floor(Math.random() * 6) + 1;
+
+console.log(`after rolling die ->`);
+newPosition = newPosition + getRandomDieValue();
+console.log(`New position is ${newPosition} `);
