ca5140a0af3d21b027dbd23c65783ee107391ac6
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-22 19:51:08 +0530

    Refactored : break down in functions and make modular

--- SnakeAndLadder.js
+++ SnakeAndLadder.js
@@ -1,16 +1,16 @@
 const startPosition = 0;
-const WinningPosition = 100;
+const winningPosition = 100;
 let newPosition = startPosition;
 var RandomDieValue = 0;
 var RandomPlayValue = 0;
-var diceRollCount=0;
-var player1Position=0;
-var player2Position=0;
+var diceRollCount = 0;
+var player1Position = 0;
+var player2Position = 0;
 var playerPosition = 0;
 
 console.log(`\n ****** Welcome to snake and ladder game ******\n`);
 console.log(`Start position is ${startPosition}`);
-console.log(`Winning position is ${WinningPosition} \n`);
+console.log(`Winning position is ${winningPosition} \n`);
 
 console.log(`after rolling die ->`);
 
@@ -18,67 +18,67 @@ getRandomPlayValue = () => Math.floor(Math.random() * 3);
 
 getRandomDieValue = () => Math.floor(Math.random() * 6) + 1;
 
-
-playGame = (playerPosition) => {
-
-    var newPosition = playerPosition;
+getDiceCount = () => {
     ++diceRollCount;
     console.log(`dice count is: ${diceRollCount}`);
+}
 
-    var randomDieValue = getRandomDieValue();
-    console.log(`die value is: ${randomDieValue}`);
-
-    var randomPlayValue = getRandomPlayValue();
-    switch(randomPlayValue) {
-
+getPlayOption = (randomPlayValue, randomDieValue, newPosition) => {
+    switch (randomPlayValue) {
         case 0:
             console.log(`no play`);
             newPosition = newPosition;
             break;
-
         case 1:
             console.log(`####### got ladder`);
+            console.log(`roll th dice again again!!`)
             newPosition = randomDieValue + playGame(newPosition);
-            if(newPosition > WinningPosition) {
-                newPosition -= randomDieValue;	
+            if (newPosition > winningPosition) {
+                newPosition -= randomDieValue;
             }
             break;
-
         case 2:
             console.log(`~~~~~~~< snake attack !!!`);
             newPosition -= randomDieValue;
-            if(newPosition < startPosition) {
+            if (newPosition < startPosition) {
                 newPosition = startPosition;
             }
             break;
-
-        default:    
-        console.log(`Something went wrong !!`);
-    }     
+        default:
+            console.log(`Something went wrong !!`);
+    }
     return newPosition;
 }
-    
-while(player1Position <= WinningPosition || player2Position <= WinningPosition) {
 
+playGame = (playerPosition) => {
+    getDiceCount();
+    var newPosition = playerPosition;
+    var randomDieValue = getRandomDieValue();
+    console.log(`die value is: ${randomDieValue}`);
+
+    var randomPlayValue = getRandomPlayValue();
+    return getPlayOption(randomPlayValue, randomDieValue, newPosition);
+}
+
+while (player1Position <= winningPosition || player2Position <= winningPosition) {
     var playerPosition = player1Position;
     var player1Position = playGame(playerPosition);
     console.log(`player 1 position is ${player1Position} \n`)
 
-    if(player1Position == WinningPosition ) {
-        console.log(`player 1 is winner`);
+    if (player1Position == winningPosition) {
+        console.log(`Hurray!!!! player 1 is winner`);
         diceRollCount = 0;
         break;
     }
 
-    
     var playerPosition = player2Position;
     var player2Position = playGame(playerPosition);
-    console.log(`player 2 position is ${player2Position} \n `)
+    console.log(` player 2 position is ${player2Position} \n `)
 
-    if(player2Position == WinningPosition ) {
-
-        console.log(`player 2 is winner`);
+    if (player2Position == winningPosition) {
+        console.log(`Hurray!!!! player 2 is winner`);
         diceRollCount = 0;
         break;
-    } 
+    }
 }
+
