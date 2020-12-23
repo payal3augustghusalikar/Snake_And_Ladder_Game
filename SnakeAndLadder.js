e9f12f9cc18f17eb22cb7f26f380809cfec192eb
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-23 21:01:51 +0530

    Added : separate function for snake case and commented the code

--- SnakeAndLadder.js
+++ SnakeAndLadder.js
@@ -1,6 +1,6 @@
-const startPosition = 0;
-const winningPosition = 100;
-let newPosition = startPosition;
+const START_POSITION = 0;
+const WINNING_POSITION = 100;
+let newPosition = START_POSITION;
 var RandomDieValue = 0;
 var RandomPlayValue = 0;
 var diceRollCount = 0;
@@ -9,15 +9,32 @@ var player2Position = 0;
 var playerPosition = 0;
 
 console.log(`\n ****** Welcome to snake and ladder game ******\n`);
-console.log(`Start position is ${startPosition}`);
-console.log(`Winning position is ${winningPosition} \n`);
+console.log(`Start position is ${START_POSITION}`);
+console.log(`Winning position is ${WINNING_POSITION} \n`);
 
 console.log(`after rolling die ->`);
 
+/**
+* @description gets random value for play option
+*/
 getRandomPlayValue = () => Math.floor(Math.random() * 3);
 
+/**
+   * @description gets random value for dice
+   */
 getRandomDieValue = () => Math.floor(Math.random() * 6) + 1;
 
+getSnake = (newPosition, randomDieValue) => {
+    console.log(`~~~~~~~< snake attack !!!`);
+    newPosition -= randomDieValue;
+    if (newPosition < START_POSITION) {
+        newPosition = START_POSITION;
+    }
+}
+
+/**
+    * @description gets dice count
+    */
 getDiceCount = () => {
     ++diceRollCount;
     console.log(`dice count is: ${diceRollCount}`);
@@ -31,18 +48,14 @@ getPlayOption = (randomPlayValue, randomDieValue, newPosition) => {
             break;
         case 1:
             console.log(`####### got ladder`);
-            console.log(`roll th dice again again!!`)
+            console.log(`roll the dice again!!`)
             newPosition = randomDieValue + playGame(newPosition);
-            if (newPosition > winningPosition) {
+            if (newPosition > WINNING_POSITION) {
                 newPosition -= randomDieValue;
             }
             break;
         case 2:
-            console.log(`~~~~~~~< snake attack !!!`);
-            newPosition -= randomDieValue;
-            if (newPosition < startPosition) {
-                newPosition = startPosition;
-            }
+            getSnake(newPosition, randomDieValue);
             break;
         default:
             console.log(`Something went wrong !!`);
@@ -50,6 +63,10 @@ getPlayOption = (randomPlayValue, randomDieValue, newPosition) => {
     return newPosition;
 }
 
+/**
+    * @description takes two player 
+    * @param {number} playerPosition
+    */
 playGame = (playerPosition) => {
     getDiceCount();
     var newPosition = playerPosition;
@@ -60,12 +77,18 @@ playGame = (playerPosition) => {
     return getPlayOption(randomPlayValue, randomDieValue, newPosition);
 }
 
-while (player1Position <= winningPosition || player2Position <= winningPosition) {
+/**
+ * @description checks winning position for player1Position and player2Position and calls play game function
+ */
+while (player1Position <= WINNING_POSITION || player2Position <= WINNING_POSITION) {
     var playerPosition = player1Position;
     var player1Position = playGame(playerPosition);
     console.log(`player 1 position is ${player1Position} \n`)
 
-    if (player1Position == winningPosition) {
+    /**
+     * @description checks player reched to winning position if true then make dice roll count to zero
+     */
+    if (player1Position == WINNING_POSITION) {
         console.log(`Hurray!!!! player 1 is winner`);
         diceRollCount = 0;
         break;
@@ -75,7 +98,11 @@ while (player1Position <= winningPosition || player2Position <= winningPosition)
     var player2Position = playGame(playerPosition);
     console.log(` player 2 position is ${player2Position} \n `)
 
-    if (player2Position == winningPosition) {
+
+    /**
+     * @description checks player reched to winning position if true then make dice roll count to zero
+     */
+    if (player2Position == WINNING_POSITION) {
         console.log(`Hurray!!!! player 2 is winner`);
         diceRollCount = 0;
         break;
